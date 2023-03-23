a7d1805788837ffb466004d2b2fab08b8183af4d
Pedro Balestra <pedro.balestra@outlook.com>
2023-03-22 22:45:23 -0300

    Criação de um botão para mostrar texto com fonte Aladin

--- lib/fonts.dart
+++ lib/fonts.dart
@@ -53,9 +53,9 @@ class _FontsState extends State<Fonts> {
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
                     borderSide: const BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(5.5)),
+                    borderRadius: BorderRadius.circular(20)),
                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.5),
+                  borderRadius: BorderRadius.circular(20),
                   borderSide: const BorderSide(color: Colors.black),
                 ),
                 labelText: "Escreva aqui",
@@ -82,10 +82,11 @@ class _FontsState extends State<Fonts> {
             child: TextFormField(
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(5.5)),
+                  borderSide: const BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.5),
+                  borderRadius: BorderRadius.circular(20),
                   borderSide: const BorderSide(color: Colors.black),
                 ),
                 labelText: "Escreva aqui",
@@ -114,9 +115,9 @@ class _FontsState extends State<Fonts> {
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
                     borderSide: const BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(5.5)),
+                    borderRadius: BorderRadius.circular(20)),
                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.5),
+                  borderRadius: BorderRadius.circular(20),
                   borderSide: const BorderSide(color: Colors.black),
                 ),
                 labelText: "Escreva aqui",
@@ -125,6 +126,65 @@ class _FontsState extends State<Fonts> {
               style: GoogleFonts.b612Mono(fontSize: 20, color: Colors.black),
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 30, right: 20),
+            child: Text(
+              "Clique no Botão para ver um exemplo de texto com a Fonte aladin",
+              style: GoogleFonts.aladin(fontSize: 20, color: Colors.black87),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: SizedBox(
+              height: 40,
+              width: 140,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text(
+                        "Aladin",
+                        style: GoogleFonts.aladin(
+                            fontSize: 20, color: Colors.black87),
+                      ),
+                      content: Text(
+                        " Ferva água em uma panela. A quantidade de água pode variar de acordo com a preferência de consistência do macarrão, mas geralmente são cerca de 300 ml a 400 ml de água para um pacote de miojo."
+                        "Abra o pacote de miojo e retire o tempero. Adicione o macarrão na água fervente e deixe cozinhar por cerca de 3 a 4 minutos, mexendo de vez em quando."
+                        "Enquanto o macarrão está cozinhando, você pode preparar o tempero. Basta adicionar o conteúdo do sachê de tempero em uma tigela ou prato fundo e adicionar um pouco de água quente. Misture bem até formar uma pasta homogênea."
+                        "Quando o macarrão estiver cozido, desligue o fogo e escorra a água. Adicione o macarrão na tigela com o tempero e misture bem até que o macarrão fique completamente coberto com o tempero.",
+                        style: GoogleFonts.aladin(
+                            fontSize: 20, color: Colors.black87),
+                      ),
+                      actions: [
+                        OutlinedButton(
+                          child: const Text('Ok'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: const Text(
+                  "Exemplo",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
